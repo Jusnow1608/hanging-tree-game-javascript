7f6b12b539b7a5851c281c4ab69fbaa56e84c572
Justyna <justyna.nowak.programista@gmail.com>
2026-07-07 15:10:31 +0200

    Added letter checking after click

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const targetPassword = "Bez pracy nie ma kołaczy".toUpperCase();
+const targetPassword = "bez pracy nie ma kołaczy".toUpperCase();
 const passwordContainer = document.querySelector('.password');
-
 
 const passwordArray = Array.from(targetPassword);
 
-let hiddenPassword = passwordArray
-  .map(char => char === " " ? " " : "-")
+let hiddenPassword = passwordArray.map(char => char === " " ? " " : "-")
   .join("");
 
 function displayPassword(){
@@ -23,8 +21,31 @@ function createKeyboard(){
     const button = document.createElement('div');
     button.classList.add('letter');
     button.textContent = letter;
+    
+    button.addEventListener('click', () => {
+      checkLetter(letter, button);
+      });
+
     alphabetContainer.appendChild(button);
   })
+}
+
+function checkLetter(letter, button){
+  
+  if (targetPassword.includes(letter)) {
+  hiddenPassword = passwordArray.map((char, index) => char === letter ? char : hiddenPassword[index])
+  .join("");
+  button.style.backgroundColor = "#003300";
+  button.style.borderColor = "#00C000";
+  button.style.color = "#00C000";
+  button.style.pointerEvents = "none";
+}
+
+else {
+  // Skucha!
+}
+
+displayPassword();
 }
 
 createKeyboard();
